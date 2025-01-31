2752cf673ee0125d87d084a4085918848c2ea586
Avinash Boruah <avinashboruah8@gmail.com>
2025-01-31 18:48:00 +0530

    simple bank contract

--- structure.sol
+++ structure.sol
@@ -3,6 +3,25 @@ pragma solidity ^0.8.7; // define the version u use
 
 // declaration of the contract
 contract structure{
-    // all codes are writen here
+   uint256 myNum; //state variable uint
+    uint256 myNumber=22;
+    string mytext="yoho"; //state variable string
+    bool apple=true; // boolean state variabel
+
+    // dive to function part
+    function set(uint256 _num) public{ //set the no
+        myNum= _num;
+    
+    }
+
+    function getNUMBER() public view returns(uint256){ //return the set no
+        return myNum;
+    }
+
+    function getstring() public view returns (string memory) { // five the dtring
+        return mytext;
+        }
+
+        
 
 }
